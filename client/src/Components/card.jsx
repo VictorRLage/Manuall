4ec1eea46de561cbaa6f8e2f5c7fba161dd9f066
Victo Lage <victor.lage@sptech.school>
2023-05-05 11:06:55 -0300

    add tailwind

---
+++ client/src/Components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function card(props) {
+    return (
+        <div className='w-96'>
+            <div class="text">
+
+            </div>
+        </div>
+        
+    );
+}
+
+export default card;
