f4a73d11814d86fd6aeeda4c8562a558a19fc6db
VictorRLage <victor.lage@sptech.school>
2023-05-30 17:00:23 -0300

    a

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -8,13 +8,13 @@ import { useNavigate } from "react-router-dom";
 function Home(props) {
     const slides = [
         {
-            url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
+            url: 'https://i.imgur.com/BQlaUcO.jpeg',
         },
         {
-            url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
+            url: 'https://i.imgur.com/BQlaUcO.jpeg',
         },
         {
-            url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+            url: 'https://i.imgur.com/BQlaUcO.jpeg',
         },
     ];
 
