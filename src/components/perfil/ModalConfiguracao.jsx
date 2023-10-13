5c59581044711835e03f678643e45dc5387bcc0d
MarcoCJR <marco.camposjr@sptech.school>
2023-10-12 21:43:57 -0300

    Modal configurações pronto, esqueci de commitar

--- src/components/perfil/ModalConfiguracao.jsx
+++ src/components/perfil/ModalConfiguracao.jsx
@@ -30,7 +30,7 @@ export default function ModalConfiguracao({
           className="bg-verde-escuro-1 text-white w-72 h-12 rounded-full mb-2 font-semibold flex justify-start items-center px-6 space-x-4"
           onClick={() => navigate("/perfil")}
         >
-          <UserIcon className="w-6 h-6 mr-2"/>
+          <UserIcon className="w-6 h-6 mr-2" />
           Perfil
         </button>
 
@@ -38,19 +38,32 @@ export default function ModalConfiguracao({
           className="bg-verde-escuro-1 text-white w-72 h-12 rounded-full mb-2 font-semibold flex justify-start items-center px-6 space-x-4"
           onClick={() => navigate("/dashboard")}
         >
-          <ChartBarIcon className="w-6 h-6 mr-2"/>
+          <ChartBarIcon className="w-6 h-6 mr-2" />
           Dashboard
         </button>
 
         <div className="bg-verde-escuro-1 text-white w-72 h-12 rounded-full mb-2 font-semibold flex justify-between items-center px-6">
           <span className="text-xl">Modo Noturno</span>
           <label className="relative flex items-center group">
-            <input type="checkbox" className="absolute z-10 w-12 h-6 appearance-none rounded-md" onChange={handleNightModeToggle} checked={isNightMode}/>
+            <input type="checkbox" className="absolute z-10 w-12 h-6 appearance-none rounded-md" onChange={handleNightModeToggle} checked={isNightMode} />
             <span className={`relative w-12 h-6 flex items-center p-0 rounded-full duration-300 ease-in-out after:w-6 after:h-6 after:bg-white after:rounded-full after:shadow-md after:duration-300 ${isNightMode ? 'bg-verde-switch after:translate-x-6' : 'bg-slate-300 after:translate-x-0'} ${!isNightMode ? 'group-hover:after:translate-x-1' : ''}`} style={{ boxShadow: "inset 0 0 5px rgba(0, 0, 0, 0.5)" }}>
               <SunIcon className={`absolute left-1 w-5 h-5 ${isNightMode ? 'opacity-0' : 'text-verde-switch'}`} />
               <MoonIcon className={`absolute right-1 w-5 h-5 ${isNightMode ? 'text-verde-switch' : 'opacity-0'}`} />
             </span>
           </label>
+        </div>
+        
+        <div className="bg-verde-escuro-1 text-white w-72 h-28 rounded-3xl mb-2 font-semibold flex flex-col justify-start items-center p-4">
+          <span className="text-xl mb-2">Modo Daltonismo</span>
+          <select 
+            className="bg-slate-300 text-verde-escuro-1 text-md font-bold border-none px-4 py-2 rounded-full cursor-pointer focus:outline-none focus:border-none"
+            onChange={(e) => console.log(e.target.value)} 
+          >
+            <option className='font-bold' value="normal">Normal</option>
+            <option className='font-bold' value="deuteranopia">Deuteranopia</option>
+            <option className='font-bold' value="protanopia">Protanopia</option>
+            <option className='font-bold' value="tritanopia">Tritanopia</option>
+          </select>
         </div>
       </div>
     </ModalCustom>
